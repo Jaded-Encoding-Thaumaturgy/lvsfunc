ef8ccc039f26c607265f6969b4a0b6b8d6fdcefe
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2019-01-21 01:42:49 +0100

    [various] Updated functions, fix minor typos, rewrite "compare"
    [General]
    - Reordered functions
    - Added more aliasses
    - Update README.md
    - Update descriptions of a couple of functions.
    
    [compare]
    - "compare" has been rewritten
    - Now instead of "[clip, clip]" it asks for "clip_a, clip_b". Sadly this removes the ability to compare more than two clips, however realistically there's no point where I've really used that myself. "frames" still works the same way.
    - Removed "match_clips". It is not up to the end-user to decide what both clips should be.
    
    [super_aa/transpose_aa]
    - Renamed "super_aa" to "transpose_aa"
    - Changed "mode" to "Eedi3". Instead of asking for a mode, you can now toggle between Nnedi3 and Eedi3 by setting "Eedi3" to "True".
    - Removed forced 16bit resampling. I leave that up to the end-user now.
    
    [denoise]
    - Added "resample" parameter. The filter will no longer force 16bit and instead make it opt-in. "True" by default.

--- README.md
+++ README.md
@@ -2,12 +2,11 @@ Contains scripts ~~stolen from other people~~ combined into a simple function fo
 
 This function offers the following:</br>
 
-- fix_eedi3(clip, strength=1, alpha=0.25, beta=0.5, gamma=40, nrad=2, mdis=20, nsize=3, nns=3, qual=1)</br>
 - compare(clips, frames, match_clips=True)</br>
-- stack_compare(clip_a, clip_b , width=None, height=None, stack_vertical=False)
-- super_aa(clip, mode=1)</br>
+- stack_compare(clip_a, clip_b , width=None, height=None, stack_vertical=False)</br>
+- transpose_aa(clip, mode=1)</br>
+- fix_eedi3(clip, strength=1, alpha=0.25, beta=0.5, gamma=40, nrad=2, mdis=20, nsize=3, nns=3, qual=1)</br>
 - denoise(clip, mode=1, bm3d=True, sigma=4, h=1.0, tr=2, refine_motion=True, sbsize=16, sosize=12)</br>
-- autoscale(clip, width=None, height=1080, kernel='bicubic', b=1/3, c=1/3, taps=4, mask_lineart=False, rfactor=None)</br>
 
 ## Requirements:
 
--- lvsfunc.py
+++ lvsfunc.py
@@ -8,7 +8,100 @@ from nnedi3_rpow2 import nnedi3_rpow2 # https://github.com/darealshinji/vapoursy
 core = vs.core
 
 # TO-DO: Write function that only masks px of a certain color/threshold of colors
-# TO-DO: Rewrite 'compare'. Should be able to handle it in a much better way than how XEL did it.
+
+
+def compare(clip_a: vs.VideoNode, clip_b: vs.VideoNode, frames):
+    """
+    Rewrite of compare. 
+    Allows for two frames to be compared by having them play right after another.
+    """
+    if clip_a.format.bits_per_sample != clip_b.format.bits_per_sample:
+        raise ValueError('compare: The bitdepth of both clips must be equal')
+    if clip_a.format.id != clip_b.format.id:
+        raise ValueError('compare: The subsampling of both clips must be equal')
+
+    final = None
+    for frame in frames:
+        A = clip_a[frame]
+        B = clip_b[frame]
+    
+        A += B
+        A if final is None else final += A
+    return final
+
+
+def stack_compare(clip_a: vs.VideoNode, clip_b: vs.VideoNode, width=None, height=None, stack_vertical=False):
+    """
+    Compares two frames by stacking.
+    Best to use when trying to match two sources frame-accurately, however by setting height to the source's 
+    height (or None), it can be used for comparing frames.
+    """
+    if clip_a.format.bits_per_sample != clip_b.format.bits_per_sample:
+        raise ValueError('stack_compare: The bitdepth of both clips must be equal')
+    if clip_a.format.id != clip_b.format.id:
+        raise ValueError('stack_compare: The subsampling of both clips must be equal')
+
+    if height is None:
+        height = clip_a.height
+    if width is None:
+        width = getw(height, ar=clip_a.width / clip_a.height)
+
+    clip_a = core.resize.Spline36(clip_a, width, height)
+    clip_b = core.resize.Spline36(clip_b, width, height)
+
+    if stack_vertical:
+        stacked = core.std.StackVertical([clip_a, clip_b])
+    else:
+        stacked = core.std.StackHorizontal([clip_a, clip_b])
+    return stacked
+
+
+
+def transpose_aa(clip: vs.VideoNode, Eedi3=False):
+    """
+    Script written by Zastin and modified by me. Useful for shows like Yuru Camp with bad lineart problems.
+    If Eedi3=False, it will use Nnedi3 instead.
+    """
+    srcY = clip.std.ShufflePlanes(0, vs.GRAY)
+
+    if eedi3:
+        def aa(srcY):
+            w, h = srcY.width, srcY.height
+            srcY = srcY.std.Transpose()
+            srcY = srcY.eedi3m.EEDI3(0, 1, 0, 0.5, 0.2)
+            srcY = srcY.znedi3.nnedi3(1, 0, 0, 3, 4, 2)
+            srcY = srcY.resize.Spline36(height, width, src_top=.5)
+            srcY = srcY.std.Transpose()
+            srcY = srcY.eedi3m.EEDI3(0, 1, 0, 0.5, 0.2)
+            srcY = srcY.znedi3.nnedi3(1, 0, 0, 3, 4, 2)
+            srcY = srcY.resize.Spline36(width, height, src_top=.5)
+            return srcY
+    else:
+        def aa(srcY):
+            srcY = srcY.std.Transpose()
+            srcY = srcY.nnedi3.nnedi3(0, 1, 0, 3, 3, 2)
+            srcY = srcY.nnedi3.nnedi3(1, 0, 0, 3, 3, 2)
+            srcY = srcY.resize.Spline36(height, width, src_top=.5)
+            srcY = srcY.std.Transpose()
+            srcY = srcY.nnedi3.nnedi3(0, 1, 0, 3, 3, 2)
+            srcY = srcY.nnedi3.nnedi3(1, 0, 0, 3, 3, 2)
+            srcY = srcY.resize.Spline36(width, height, src_top=.5)
+            return srcY
+
+    def csharp(flt, src):
+        blur = core.std.Convolution(flt, [1] * 9)
+        return core.std.Expr([flt, src, blur], 'x y < x x + z - x max y min x x + z - x min y max ?')
+
+    aaclip = aa(srcY)
+    aaclip = csharp(aaclip, srcY).rgvs.Repair(srcY, 13)
+
+    if clip.format.color_family is vs.GRAY:
+        return aaclip
+    else:
+        U = clip.std.ShufflePlanes(1, vs.GRAY)
+        V = clip.std.ShufflePlanes(2, vs.GRAY)
+        merged = core.std.ShufflePlanes([aaclip, U, V], 0, vs.YUV)
+        return merged
 
 
 def fix_eedi3(clip: vs.VideoNode, strength=1, alpha=0.25, beta=0.5, gamma=40, nrad=2, mdis=20, nsize=3, nns=3, qual=1):
@@ -31,152 +124,27 @@ def fix_eedi3(clip: vs.VideoNode, strength=1, alpha=0.25, beta=0.5, gamma=40, nr
                                                                                                                planes=0)
     return clip.std.MaskedMerge(aa, mask, planes=0)
 
-
-def compare(clips: vs.VideoNode, frames, match_clips=True):
+def denoise(clip: vs.VideoNode, mode=1, bm3d=True, sigma=3, h=1.0, refine_motion=True, sbsize=16, resample=True):
     """
-    Script written XEL8o9 and slightly modified by me. Grabs a given frame from two clips for easier comparison.
-    Intended order is [src, filtered].
-    
-    Example use:
-    lvf.compare([src, filtered], [10, 20, 30, 40, 50], match_clips=True)
-    """
-    if len(clips) < 2:
-        raise ValueError('compare: There must be at least two clips')
-    width = clips[0].width
-    height = clips[0].height
-    for i in range(0, len(clips)):
-        if clips[i].format.bits_per_sample != 16:
-            clips[i] = fvf.Depth(clips[i], 16)
-    final = None
-    for frame in frames:
-        newClip = core.std.Trim(clips[0], frame, frame)
-        for i in range(1, len(clips)):
-            nextFrame = core.std.Trim(clips[i], frame, frame)
-            if nextFrame.width != width or nextFrame.height != height:
-                if match_clips:
-                    nextFrame = core.resize.Spline36(nextFrame, width, height)
-                else:
-                    raise ValueError('compare: The dimensions of each clip must be equal')
-            newClip += nextFrame
-        if final is None:
-            final = newClip
-        else:
-            final += newClip
-    return final
-
-
-def stack_compare(clip_a: vs.VideoNode, clip_b: vs.VideoNode, width=None, height=None, stack_vertical=False):
-    """
-    Compares two frames by stacking.
-    Best to use when trying to match two sources frame-accurately, however by setting height to the source's height (or None) it can be used for comparisons that way.
-    """
-    if clip_a.format.bits_per_sample != clip_b.format.bits_per_sample:
-        raise ValueError('stack_compare: The bitdepth of both clips must be equal')
-    if clip_a.format.id != clip_b.format.id:
-        raise ValueError('stack:compare: The subsampling of both clips must be equal')
-
-    if height is None:
-        height = clip_a.height
-    if width is None:
-        width = getw(height, ar=clip_a.width / clip_a.height)
-
-    clip_a = core.resize.Spline36(clip_a, width, height)
-    clip_b = core.resize.Spline36(clip_b, width, height)
-
-    if stack_vertical:
-        stacked = core.std.StackVertical([clip_a, clip_b])
-    else:
-        stacked = core.std.StackHorizontal([clip_a, clip_b])
-
-    return stacked
-
-
-
-def super_aa(clip: vs.VideoNode, width=None, Height=None, mode=1):
-    """
-    Script written by Zastin and modified by me. Useful for shows like Yuru Camp with bad lineart problems.
-    
-    Mode 1 = Nnedi3 
-    Mode 2 = Eedi3
-    """
-    if clip.format.bits_per_sample != 16:
-        clip = fvf.Depth(clip, 16)
-    srcY = clip.std.ShufflePlanes(0, vs.GRAY)
-
-    if height is None:
-        height = srcY.height
-    if width is None:
-        width = getw(height, ar=srcY.width / srcY.height)
-
-    if mode is 1:
-        def aa(srcY):
-            srcY = srcY.std.Transpose()
-            srcY = srcY.nnedi3.nnedi3(0, 1, 0, 3, 3, 2)
-            srcY = srcY.nnedi3.nnedi3(1, 0, 0, 3, 3, 2)
-            srcY = srcY.resize.Spline36(height, width, src_top=.5)
-            srcY = srcY.std.Transpose()
-            srcY = srcY.nnedi3.nnedi3(0, 1, 0, 3, 3, 2)
-            srcY = srcY.nnedi3.nnedi3(1, 0, 0, 3, 3, 2)
-            srcY = srcY.resize.Spline36(width, height, src_top=.5)
-            return srcY
-    elif mode is 2:
-        def aa(srcY):
-            w, h = srcY.width, srcY.height
-            srcY = srcY.std.Transpose()
-            srcY = srcY.eedi3m.EEDI3(0, 1, 0, 0.5, 0.2)
-            srcY = srcY.znedi3.nnedi3(1, 0, 0, 3, 4, 2)
-            srcY = srcY.resize.Spline36(height, width, src_top=.5)
-            srcY = srcY.std.Transpose()
-            srcY = srcY.eedi3m.EEDI3(0, 1, 0, 0.5, 0.2)
-            srcY = srcY.znedi3.nnedi3(1, 0, 0, 3, 4, 2)
-            srcY = srcY.resize.Spline36(width, height, src_top=.5)
-            return srcY
-    else:
-        raise ValueError('super_aa: unknown mode')
-
-    def csharp(flt, src):
-        blur = core.std.Convolution(flt, [1] * 9)
-        return core.std.Expr([flt, src, blur], 'x y < x x + z - x max y min x x + z - x min y max ?')
-
-    aaclip = aa(srcY)
-    aaclip = csharp(aaclip, srcY).rgvs.Repair(srcY, 13)
-
-    if clip.format.color_family is vs.GRAY:
-        return aaclip
-    else:
-        srcU = clip.std.ShufflePlanes(1, vs.GRAY)
-        srcV = clip.std.ShufflePlanes(2, vs.GRAY)
-        merged = core.std.ShufflePlanes([aaclip, srcU, srcV], 0, vs.YUV)
-        return merged
-
-def getw(h, ar=16 / 9, only_even=True):
-    """
-    returns width for image (taken from kagefunc)
-    """
-    w = h * ar
-    w = int(round(w))
-    if only_even:
-        w = w // 2 * 2
-    return w
-
-def denoise(clip: vs.VideoNode, mode=1, bm3d=True, sigma=3, h=1.0, refine_motion=True, sbsize=16):
-    """
-    Generic denoising. Denoising is done by BM3D with other denoisers being used for ref. Returns the denoised clip used
+    Wrapper for generic denoising. Denoising is done by BM3D with a given denoisers being used for ref. Returns the denoised clip used
     as ref if BM3D=False.
 
     Mode 1 = KNLMeansCL
     Mode 2 = SMDegrain
     Mode 3 = DFTTest
+
+    Will be removed eventuallyTM.
     """
-    if clip.format.bits_per_sample != 16:
-        clip = fvf.Depth(clip, 16)
+    if resample:
+        if clip.format.bits_per_sample != 16:
+            clip = fvf.Depth(clip, 16)
     clipY = core.std.ShufflePlanes(clip, 0, vs.GRAY)
 
-    if mode == 1 or knlm:
+    if mode == 1 or 'knlm':
         denoiseY = clipY.knlm.KNLMeansCL(d=3, a=2, h=h)
-    elif mode == 2 or SMD:
+    elif mode == 2 or 'SMD' or 'SMDegrain':
         denoiseY = haf.SMDegrain(clipY, prefilter=3, RefineMotion=refine_motion)
-    elif mode == 3 or DFT:
+    elif mode == 3 or 'DFT' or 'dfttest':
         denoiseY = clipY.dfttest.DFTTest(sigma=4.0, tbsize=1, sbsize=sbsize, sosize=sbsize*0.75)
     else:
         raise ValueError('denoise: unknown mode')
@@ -196,21 +164,21 @@ def denoise(clip: vs.VideoNode, mode=1, bm3d=True, sigma=3, h=1.0, refine_motion
         return merged
 
 
-def Source(path_to_clip, mode='lsmas', resample=False):
+def Source(source, mode='lsmas', resample=False):
     """
     Just a stupid import script. There really is no reason to use this, but hey, it was fun to write.
     """
 
-    if path_to_clip.startswith("file:///"):
-        path_to_clip = path_to_clip[8::]
+    if source.startswith("file:///"):
+        source = source[8::]
 
-    if path_to_clip.endswith(".d2v"):
-        src = core.d2v.Source(path_to_clip)
+    if source.endswith(".d2v"):
+        src = core.d2v.Source(source)
     else:
         if mode == 1 or 'lsmas':
-            src = core.lsmas.LWLibavSource(path_to_clip)
+            src = core.lsmas.LWLibavSource(source)
         elif mode == 2 or 'ffms2':
-            src = core.ffms2.Source(path_to_clip)
+            src = core.ffms2.Source(source)
         else:
             raise ValueError('source: Unknown mode')
       
@@ -219,5 +187,23 @@ def Source(path_to_clip, mode='lsmas', resample=False):
 
     return src
 
-# Source alias (for additional autism)
+
+# Aliasses
 src = Source
+comp = compare
+s_comp = stack_compare
+
+# Helper functions:
+
+
+# Helper functions written by other people:
+
+def getw(h, ar=16 / 9, only_even=True): # Credit to kageru for writing this
+    """
+    returns width for image (taken from kagefunc)
+    """
+    w = h * ar
+    w = int(round(w))
+    if only_even:
+        w = w // 2 * 2
+    return w
