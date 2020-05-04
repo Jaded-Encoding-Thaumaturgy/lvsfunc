3f0f9382932cc4dfd74fc2f5406aa6b26885c5ee
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2020-05-04 10:50:41 +0200

    Update version to 0.0.4
    Maybe one day I'll learn how to actually use versions.

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ with open("requirements.txt") as fh:
     install_requires = fh.read()
 
 name = "lvsfunc"
-version = "0.0.3"
-release = "0.0.3"
+version = "0.0.4"
+release = "0.0.4"
 
 setuptools.setup(
     name=name,
