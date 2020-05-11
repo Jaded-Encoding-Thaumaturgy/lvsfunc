10423def9f379204dcc092221e5968b84cfcb632
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2020-05-11 18:24:31 +0200

    scale: detail_mask: fix detail_mask accepting multiple frame formats

--- lvsfunc/scale.py
+++ lvsfunc/scale.py
@@ -130,6 +130,8 @@ def detail_mask(clip: vs.VideoNode, rescaled_clip: vs.VideoNode,
 
     :return:               Mask of lost detail
     """
+    if clip.format.id != rescaled_clip.format.id:
+            raise ValueError(f"detail_mask: 'The format of both clips must be equal'")
     mask = core.std.Expr([clip, rescaled_clip], 'x y - abs') \
         .std.Binarize(threshold)
     mask = iterate(mask, core.std.Maximum, 4)
