a38cf93d5efad30661032d9f8c1862051d2b5200
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2021-06-18 04:58:24 +0200

    bump version
    Changelog:
    New Additions:
    * Add named Bicubic kernels (BSpline, Hermite, Mitchell, Catrom, BicubicSharp, RobidouxSoft, Robidoux, RobidouxSharp)
    * Add a keyframe generator (render.find_scene_changes)
        - Outputs a list of scenechanges determined by wwxd, scxvid, frames only found by both, or frames found by either.
    
    Updates to functions:
    * Update dehalo.bidehalo
        - Remove masking, this is now up to the user to handle
        - Fix bug where float clips would error because bilateral can't handle them
    * Source
        - Fix a bug where the improper Matrix params would be passed (int to str param instead of int param) which would break clips if you used `ref`

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ with open("requirements.txt") as fh:
     install_requires = fh.read()
 
 name = "lvsfunc"
-version = "0.3.4"
-release = "0.3.4"
+version = "0.3.5"
+release = "0.3.5"
 
 setuptools.setup(
     name=name,
