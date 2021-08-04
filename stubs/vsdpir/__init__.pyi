8bba49e2f1c569a506b44bb7c8c865dd16e2b64d
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2021-08-05 01:20:00 +0200

    Update __init__.pyi

--- stubs/vsdpir/__init__.pyi
+++ stubs/vsdpir/__init__.pyi
@@ -1,6 +1,11 @@
 import vapoursynth as vs
 
+from . import network_unet, utils_model
+
 from typing import Optional
 
 def DPIR(clip: vs.VideoNode, strength: Optional[float] = None, task: str = 'denoise',
          device_type: str = 'cuda', device_index: int = 0) -> vs.VideoNode: ...
+
+def frame_to_tensor(f: vs.VideoFrame) -> torch.Tensor: ...
+def tensor_to_frame(t: torch.Tensor, f: vs.VideoFrame) -> vs.VideoFrame: ...
