be876aca681a8ef5cbd829bbfa34a983e141b1f4
Ichunjo <ichunjo.le.terrible@gmail.com>
2021-08-06 23:05:47 +0200

    update autodb_dpir

--- lvsfunc/deblock.py
+++ lvsfunc/deblock.py
@@ -2,21 +2,22 @@
     Deblocking functions.
 """
 from functools import partial
-from typing import List, Optional, Sequence, Tuple, cast
+from typing import Optional, Sequence, Tuple
 
 import vapoursynth as vs
-from vsutil import scale_value
 
 from .types import Matrix
+from .util import get_prop
 
 core = vs.core
 
 
 def autodb_dpir(clip: vs.VideoNode, edgevalue: int = 24,
-                strs: Sequence[float] = (30, 50, 75),
+                strs: Sequence[float] = [30, 50, 75],
                 thrs: Sequence[Tuple[float, float, float]] = [(1.5, 2.0, 2.0), (3.0, 4.5, 4.5), (5.5, 7.0, 7.0)],
                 matrix: Optional[Matrix] = None,
                 cuda: bool = True, device_index: int = 0,
+                write_props: bool = False
                 ) -> vs.VideoNode:
     """
     A rewrite of fvsfunc.AutoDeblock that uses vspdir instead of dfttest to deblock.
@@ -52,6 +53,7 @@ def autodb_dpir(clip: vs.VideoNode, edgevalue: int = 24,
                             If `None`, gets matrix from the "_Matrix" prop of the clip
     :param cuda:            Device type used for deblocking. Uses CUDA if True, else CPU
     :param device_index:    The 'device_index' + 1º device of type device type in the system
+    :write_props            Will write verbose props
 
     :return:                Deblocked clip
     """
@@ -60,34 +62,36 @@ def autodb_dpir(clip: vs.VideoNode, edgevalue: int = 24,
     except ModuleNotFoundError:
         raise ModuleNotFoundError("autodb_dpir: missing dependency 'vsdpir'")
 
-    assert clip.format
+    if clip.format is None:
+        raise ValueError("autodb_dpir: 'Variable-format clips not supported'")
 
-    def _eval_db(n: int, f: List[vs.VideoFrame], clips: List[vs.VideoNode]) -> vs.VideoNode:
-        out = clips[0]
-        deblocked = False
+    def _eval_db(n: int, f: Sequence[vs.VideoFrame],
+                 clip: vs.VideoNode, db_clips: Sequence[vs.VideoNode],
+                 nthrs: Sequence[Tuple[float, float, float]]) -> vs.VideoNode:
 
-        evref_diff = scale_value(cast(float, f[0].props['EdgeValRefDiff']), 32, 8)
-        y_next_diff = scale_value(cast(float, f[1].props['YNextDiff']), 32, 8)
-        y_prev_diff = scale_value(cast(float, f[2].props['YPrevDiff']), 32, 8)
-        f_type = cast(bytes, f[0].props['_PictType']).decode('utf-8')
+        evref_diff, y_next_diff, y_prev_diff = [
+            get_prop(f[i], prop, float)
+            for i, prop in zip(range(3), ['EdgeValRefDiff', 'YNextDiff', 'YPrevDiff'])
+        ]
+        f_type = get_prop(f[0], '_PictType', bytes).decode('utf-8')
 
         if f_type == 'I':
             y_next_diff = (y_next_diff + evref_diff) / 2
 
-        for i, thr in enumerate(thrs):
-            if evref_diff > thr[0] and y_next_diff > thr[1] and y_prev_diff > thr[2]:
-                out, i = clips[i+1], i
-                deblocked = True
+        out = clip
+        nthr_used = (-1., ) * 3
+        for dblk, nthr in zip(db_clips, nthrs):
+            if all(p > t for p, t in zip([evref_diff, y_next_diff, y_prev_diff], nthr)):
+                out = dblk
+                nthr_used = nthr
 
-        out = out.std.SetFrameProp('adb_EdgeValRefDiff', floatval=evref_diff) \
-            .std.SetFrameProp('adb_YNextDiff', floatval=y_next_diff) \
-            .std.SetFrameProp('adb_YPrevDiff', floatval=y_prev_diff)
-
-        if deblocked:
-            # TO-DO: Figure out why it doesn't appear to be adding these props
-            out.std.SetFrameProp('adb_EdgeValRefDiff_threshold', floatval=thrs[i][0]) \
-                .std.SetFrameProp('adb_YNextDiff_threshold', floatval=thrs[i][1]) \
-                .std.SetFrameProp('adb_YPrevDiff_threshold', floatval=thrs[i][2])
+        if write_props:
+            for prop_name, prop_val in zip(
+                ['AdbEdgeValRefDiff', 'AdbYNextDiff', 'AdbYPrevDiff',
+                 'AdbEdgeValRefDiffThreshold', 'AdbYNextDiffThreshold', 'AdbYPrevDiffThreshold'],
+                [evref_diff, y_next_diff, y_prev_diff] + list(nthr_used)
+            ):
+                out = out.std.SetFrameProp(prop_name, floatval=prop_val)
 
         return out
 
@@ -95,7 +99,9 @@ def autodb_dpir(clip: vs.VideoNode, edgevalue: int = 24,
         raise ValueError('autodb_dpir: You must pass an equal amount of values to '
                          f'strenght {len(strs)} and thrs {len(thrs)}!')
 
-    rgb = core.resize.Bicubic(clip, format=vs.RGBS, matrix_in=matrix)
+    nthrs = [tuple(x / 255 for x in thr) for thr in thrs]
+
+    rgb = core.resize.Bicubic(clip, format=vs.RGBS, matrix_in=matrix).std.SetFrameProp('AdbDeblockStrength', floatval=0)
 
     assert rgb.format
 
@@ -108,9 +114,14 @@ def autodb_dpir(clip: vs.VideoNode, edgevalue: int = 24,
     diffnext = core.std.PlaneStats(rgb, rgb.std.DeleteFrames([0]), prop='YNext')
     diffprev = core.std.PlaneStats(rgb, rgb[0] + rgb, prop='YPrev')
 
-    db_clips = [rgb] + [DPIR(rgb, strength=st, task='deblock', device_type='cuda' if cuda else 'cpu',
-                        device_index=device_index).std.SetFrameProp('adb_DeblockStrength', floatval=st) for st in strs]
+    db_clips = [
+        DPIR(rgb, strength=st, task='deblock', device_type='cuda' if cuda else 'cpu',
+             device_index=device_index).std.SetFrameProp('AdbDeblockStrength', floatval=st) for st in strs
+    ]
 
-    debl = core.std.FrameEval(rgb, partial(_eval_db, clips=db_clips), prop_src=[diffevref, diffnext, diffprev])
+    debl = core.std.FrameEval(
+        rgb, partial(_eval_db, clip=rgb, db_clips=db_clips, nthrs=nthrs),
+        prop_src=[diffevref, diffnext, diffprev]
+    )
 
     return core.resize.Bicubic(debl, format=clip.format.id, matrix=matrix)
