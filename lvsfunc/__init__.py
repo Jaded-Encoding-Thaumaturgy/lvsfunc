ce0fa3b80dfe47b4a83b74e3e9907241242da32f
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2021-10-17 23:41:08 +0200

    Update init docstring (updates docs' about)

--- lvsfunc/__init__.py
+++ lvsfunc/__init__.py
@@ -3,6 +3,8 @@
 
     If you spot any issues, please do not hesitate to send in a Pull Request
     or reach out to me on Discord (LightArrowsEXE#0476)!
+
+    For further support, drop by `#lvsfunc` in the `IEW Discord server <https://discord.gg/qxTxVJGtst>`_.
 """
 
 # flake8: noqa
