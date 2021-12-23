a698c1caeff9d2b447d4ef9f1725f01af8756a1a
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2021-12-23 23:56:34 +0100

    bump version

--- docs/conf.py
+++ docs/conf.py
@@ -21,10 +21,10 @@ copyright = '2021, LightArrowsEXE'
 author = 'LightArrowsEXE'
 
 # The short X.Y version
-version = '0.3.10'
+version = '0.3.11'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.10'
+release = '0.3.11'
 
 # -- General configuration ---------------------------------------------------
 
