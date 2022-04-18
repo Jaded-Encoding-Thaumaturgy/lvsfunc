4b5aa1624dd38471f6fcb831dcb4a3c2290c6bac
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2022-04-18 08:00:55 +0200

    docs: Update kernel page
    * Add a list of common methods
    * Add an example kernel class with method docstrings

--- docs/submodules/kernels.rst
+++ docs/submodules/kernels.rst
@@ -32,6 +32,35 @@ This allows for easy customizability, and every kernel can be given unique param
 
 Using this interface allows for consistency, which makes supporting a wide array of kernels in your own function very simple.
 
+Methods
+^^^^^^^
+
+Every `Kernel` class comes with a set of methods:
+
+.. autoclass:: lvsfunc.kernels.Example.scale
+    :members:
+    :undoc-members:
+    :show-inheritance:
+    :member-order: bysource
+
+.. autoclass:: lvsfunc.kernels.Example.descale
+    :members:
+    :undoc-members:
+    :show-inheritance:
+    :member-order: bysource
+
+.. autoclass:: lvsfunc.kernels.Example.shift
+    :members:
+    :undoc-members:
+    :show-inheritance:
+    :member-order: bysource
+
+.. autoclass:: lvsfunc.kernels.Example.resample
+    :members:
+    :undoc-members:
+    :show-inheritance:
+    :member-order: bysource
+
 Supporting Kernels in Your Own Function
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
@@ -72,5 +101,5 @@ All Available Kernels
 
 .. automodule:: lvsfunc.kernels
     :members:
-    :undoc-members:
     :show-inheritance:
+    :exclude-members: Example
--- lvsfunc/kernels.py
+++ lvsfunc/kernels.py
@@ -275,6 +275,77 @@ class Spline64(Kernel):
                                     **self.kwargs)
 
 
+class Example(Kernel):
+    """
+    Example Kernel class for documentation purposes.
+    """
+    def __init__(self, b: float = 0, c: float = 1/2, **kwargs: Any) -> None:
+        self.b = b
+        self.c = c
+        super().__init__(**kwargs)
+
+    def scale(self, clip: vs.VideoNode, width: int, height: int,
+              shift: Tuple[float, float] = (0, 0)) -> vs.VideoNode:
+        """
+        Perform a regular scaling operation
+
+        :param clip:        Input clip
+        :param width:       Output width
+        :param height:      Output height
+        :param shift:       Shift clip during the operation.
+                            Expects a tuple of (src_top, src_left).
+        """
+        return core.resize.Bicubic(clip, width, height,
+                                   src_top=shift[0], src_left=shift[1],
+                                   filter_param_a=self.b, filter_param_b=self.c,
+                                   **self.kwargs)
+
+    def descale(self, clip: vs.VideoNode, width: int, height: int,
+                shift: Tuple[float, float] = (0, 0)) -> vs.VideoNode:
+        """
+        Perform a regular descaling operation
+
+        :param clip:        Input clip
+        :param width:       Output width
+        :param height:      Output height
+        :param shift:       Shift clip during the operation.
+                            Expects a tuple of (src_top, src_left).
+        """
+        return core.descale.Debicubic(clip, width, height, b=self.b,
+                                      c=self.c, src_top=shift[0],
+                                      src_left=shift[1])
+
+    def resample(self, clip: vs.VideoNode, format: vs.PresetFormat | vs.VideoFormat,
+                 matrix: vs.MatrixCoefficients | None = None,
+                 matrix_in: vs.MatrixCoefficients | None = None) -> vs.VideoNode:
+        """
+        Perform a regular resampling operation
+
+        :param clip:        Input clip
+        :param format:      Output format
+        :param matrix:      Output matrix. If `None`, will take the matrix from the input clip's frameprops.
+        :param matrix_in:   Input matrix. If `None`, will take the matrix from the input clip's frameprops.
+
+        :rtype:             ``VideoNode``
+        """
+        return core.resize.Bicubic(clip, format=int(format),
+                                   filter_param_a=self.b, filter_param_b=self.c,
+                                   matrix=matrix, matrix_in=matrix_in,
+                                   **self.kwargs)
+
+    def shift(self, clip: vs.VideoNode, shift: Tuple[float, float] = (0, 0)) -> vs.VideoNode:
+        """
+        Perform a regular shifting operation
+
+        :param clip:        Input clip
+        :param shift:       Shift clip during the operation.
+                            Expects a tuple of (src_top, src_left).
+        """
+        return core.resize.Bicubic(clip, src_top=shift[0], src_left=shift[1],
+                                   filter_param_a=self.b, filter_param_b=self.c,
+                                   **self.kwargs)
+
+
 class BSpline(Bicubic):
     """
     Bicubic b=1, c=0
