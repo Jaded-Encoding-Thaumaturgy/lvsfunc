e1e12d174072fa7dc48359e6936b40b95a808f46
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2022-06-08 01:21:37 +0200

    vsdpir: Move to the top of the submodule

--- lvsfunc/deblock.py
+++ lvsfunc/deblock.py
@@ -17,119 +17,6 @@ __all__: List[str] = [
 ]
 
 
-def autodb_dpir(clip: vs.VideoNode, edgevalue: int = 24,
-                strs: Sequence[float] = [30, 50, 75],
-                thrs: Sequence[Tuple[float, float, float]] = [(1.5, 2.0, 2.0), (3.0, 4.5, 4.5), (5.5, 7.0, 7.0)],
-                matrix: Optional[Matrix | int] = None,
-                cuda: bool = True,
-                write_props: bool = False,
-                **vsdpir_args: Any) -> vs.VideoNode:
-    """
-    Rewrite of fvsfunc.AutoDeblock that uses vspdir instead of dfttest to deblock.
-
-    This function checks for differences between a frame and an edge mask with some processing done on it,
-    and for differences between the current frame and the next frame.
-    For frames where both thresholds are exceeded, it will perform deblocking at a specified strength.
-    This will ideally be frames that show big temporal *and* spatial inconsistencies.
-
-    Thresholds and calculations are added to the frameprops to use as a reference when setting the thresholds.
-
-    Thanks Vardë, louis, setsugen_no_ao!
-
-    Dependencies:
-
-    * vs-dpir
-
-    :param clip:            Clip to process.
-    :param edgevalue:       Remove edges from the edge mask that exceed this threshold
-                            (higher means more edges removed).
-    :param strs:            A list of DPIR strength values (higher means stronger deblocking).
-                            You can pass any arbitrary number of values here.
-                            Sane deblocking strengths lie between 1–20 for most regular deblocking.
-                            Going higher than 50 is not recommended outside of very extreme cases.
-                            The amount of values in strs and thrs need to be equal.
-    :param thrs:            A list of thresholds, written as [(EdgeValRef, NextFrameDiff, PrevFrameDiff)].
-                            You can pass any arbitrary number of values here.
-                            The amount of values in strs and thrs need to be equal.
-    :param matrix:          Enum for the matrix of the Clip to process. See ``types.Matrix`` for more info.
-                            If `None`, gets matrix from the "_Matrix" prop of the clip unless it's an RGB clip,
-                            in which case it stays as `None`.
-    :param cuda:            Use CUDA backend if True, else CPU backend.
-    :param write_props:     Will write verbose props.
-    :param vsdpir_args:     Additional args to pass to ``vsdpir``.
-
-    :return:                Deblocked clip.
-    """
-    check_variable(clip, "autodb_dpir")
-    assert clip.format
-
-    def _eval_db(n: int, f: Sequence[vs.VideoFrame],
-                 clip: vs.VideoNode, db_clips: Sequence[vs.VideoNode],
-                 nthrs: Sequence[Tuple[float, float, float]]) -> vs.VideoNode:
-
-        evref_diff, y_next_diff, y_prev_diff = [
-            get_prop(f[i], prop, float)
-            for i, prop in zip(range(3), ['EdgeValRefDiff', 'YNextDiff', 'YPrevDiff'])
-        ]
-        f_type = get_prop(f[0], '_PictType', bytes).decode('utf-8')
-
-        if f_type == 'I':
-            y_next_diff = (y_next_diff + evref_diff) / 2
-
-        out = clip
-        nthr_used = (-1., ) * 3
-        for dblk, nthr in zip(db_clips, nthrs):
-            if all(p > t for p, t in zip([evref_diff, y_next_diff, y_prev_diff], nthr)):
-                out = dblk
-                nthr_used = nthr
-
-        if write_props:
-            for prop_name, prop_val in zip(
-                ['Adb_EdgeValRefDiff', 'Adb_YNextDiff', 'Adb_YPrevDiff',
-                 'Adb_EdgeValRefDiffThreshold', 'Adb_YNextDiffThreshold', 'Adb_YPrevDiffThreshold'],
-                [evref_diff, y_next_diff, y_prev_diff] + list(nthr_used)
-            ):
-                out = out.std.SetFrameProp(prop_name, floatval=max(prop_val * 255, -1))
-
-        return out
-
-    if len(strs) != len(thrs):
-        raise ValueError('autodb_dpir: You must pass an equal amount of values to '
-                         f'strenght {len(strs)} and thrs {len(thrs)}!')
-
-    nthrs = [tuple(x / 255 for x in thr) for thr in thrs]
-
-    is_rgb = clip.format.color_family is vs.RGB
-
-    if not matrix and not is_rgb:
-        matrix = get_prop(clip.get_frame(0), "_Matrix", int)
-
-    rgb = core.resize.Bicubic(clip, format=vs.RGBS, matrix_in=matrix) if not is_rgb else clip
-
-    assert rgb.format
-
-    maxvalue = (1 << rgb.format.bits_per_sample) - 1
-    evref = core.std.Prewitt(rgb)
-    evref = core.std.Expr(evref, f"x {edgevalue} >= {maxvalue} x ?")
-    evref_rm = evref.std.Median().std.Convolution(matrix=[1, 2, 1, 2, 4, 2, 1, 2, 1])
-
-    diffevref = core.std.PlaneStats(evref, evref_rm, prop='EdgeValRef')
-    diffnext = core.std.PlaneStats(rgb, rgb.std.DeleteFrames([0]), prop='YNext')
-    diffprev = core.std.PlaneStats(rgb, rgb[0] + rgb, prop='YPrev')
-
-    db_clips = [
-        vsdpir(rgb, strength=st, mode='deblock', cuda=cuda, **vsdpir_args)
-        .std.SetFrameProp('Adb_DeblockStrength', intval=int(st)) for st in strs
-    ]
-
-    debl = core.std.FrameEval(
-        rgb, partial(_eval_db, clip=rgb, db_clips=db_clips, nthrs=nthrs),
-        prop_src=[diffevref, diffnext, diffprev]
-    )
-
-    return core.resize.Bicubic(debl, format=clip.format.id, matrix=matrix if not is_rgb else None)
-
-
 def vsdpir(clip: vs.VideoNode,
            strength: VSDPIR_STRENGTH_TYPE = 25,
            mode: str = 'deblock',
@@ -293,3 +180,116 @@ def vsdpir(clip: vs.VideoNode,
         return depth(run_dpir, bit_depth)
 
     return kernel.resample(run_dpir, targ_format, targ_matrix)
+
+
+def autodb_dpir(clip: vs.VideoNode, edgevalue: int = 24,
+                strs: Sequence[float] = [30, 50, 75],
+                thrs: Sequence[Tuple[float, float, float]] = [(1.5, 2.0, 2.0), (3.0, 4.5, 4.5), (5.5, 7.0, 7.0)],
+                matrix: Optional[Matrix | int] = None,
+                cuda: bool = True,
+                write_props: bool = False,
+                **vsdpir_args: Any) -> vs.VideoNode:
+    """
+    Rewrite of fvsfunc.AutoDeblock that uses vspdir instead of dfttest to deblock.
+
+    This function checks for differences between a frame and an edge mask with some processing done on it,
+    and for differences between the current frame and the next frame.
+    For frames where both thresholds are exceeded, it will perform deblocking at a specified strength.
+    This will ideally be frames that show big temporal *and* spatial inconsistencies.
+
+    Thresholds and calculations are added to the frameprops to use as a reference when setting the thresholds.
+
+    Thanks Vardë, louis, setsugen_no_ao!
+
+    Dependencies:
+
+    * vs-dpir
+
+    :param clip:            Clip to process.
+    :param edgevalue:       Remove edges from the edge mask that exceed this threshold
+                            (higher means more edges removed).
+    :param strs:            A list of DPIR strength values (higher means stronger deblocking).
+                            You can pass any arbitrary number of values here.
+                            Sane deblocking strengths lie between 1–20 for most regular deblocking.
+                            Going higher than 50 is not recommended outside of very extreme cases.
+                            The amount of values in strs and thrs need to be equal.
+    :param thrs:            A list of thresholds, written as [(EdgeValRef, NextFrameDiff, PrevFrameDiff)].
+                            You can pass any arbitrary number of values here.
+                            The amount of values in strs and thrs need to be equal.
+    :param matrix:          Enum for the matrix of the Clip to process. See ``types.Matrix`` for more info.
+                            If `None`, gets matrix from the "_Matrix" prop of the clip unless it's an RGB clip,
+                            in which case it stays as `None`.
+    :param cuda:            Use CUDA backend if True, else CPU backend.
+    :param write_props:     Will write verbose props.
+    :param vsdpir_args:     Additional args to pass to ``vsdpir``.
+
+    :return:                Deblocked clip.
+    """
+    check_variable(clip, "autodb_dpir")
+    assert clip.format
+
+    def _eval_db(n: int, f: Sequence[vs.VideoFrame],
+                 clip: vs.VideoNode, db_clips: Sequence[vs.VideoNode],
+                 nthrs: Sequence[Tuple[float, float, float]]) -> vs.VideoNode:
+
+        evref_diff, y_next_diff, y_prev_diff = [
+            get_prop(f[i], prop, float)
+            for i, prop in zip(range(3), ['EdgeValRefDiff', 'YNextDiff', 'YPrevDiff'])
+        ]
+        f_type = get_prop(f[0], '_PictType', bytes).decode('utf-8')
+
+        if f_type == 'I':
+            y_next_diff = (y_next_diff + evref_diff) / 2
+
+        out = clip
+        nthr_used = (-1., ) * 3
+        for dblk, nthr in zip(db_clips, nthrs):
+            if all(p > t for p, t in zip([evref_diff, y_next_diff, y_prev_diff], nthr)):
+                out = dblk
+                nthr_used = nthr
+
+        if write_props:
+            for prop_name, prop_val in zip(
+                ['Adb_EdgeValRefDiff', 'Adb_YNextDiff', 'Adb_YPrevDiff',
+                 'Adb_EdgeValRefDiffThreshold', 'Adb_YNextDiffThreshold', 'Adb_YPrevDiffThreshold'],
+                [evref_diff, y_next_diff, y_prev_diff] + list(nthr_used)
+            ):
+                out = out.std.SetFrameProp(prop_name, floatval=max(prop_val * 255, -1))
+
+        return out
+
+    if len(strs) != len(thrs):
+        raise ValueError('autodb_dpir: You must pass an equal amount of values to '
+                         f'strenght {len(strs)} and thrs {len(thrs)}!')
+
+    nthrs = [tuple(x / 255 for x in thr) for thr in thrs]
+
+    is_rgb = clip.format.color_family is vs.RGB
+
+    if not matrix and not is_rgb:
+        matrix = get_prop(clip.get_frame(0), "_Matrix", int)
+
+    rgb = core.resize.Bicubic(clip, format=vs.RGBS, matrix_in=matrix) if not is_rgb else clip
+
+    assert rgb.format
+
+    maxvalue = (1 << rgb.format.bits_per_sample) - 1
+    evref = core.std.Prewitt(rgb)
+    evref = core.std.Expr(evref, f"x {edgevalue} >= {maxvalue} x ?")
+    evref_rm = evref.std.Median().std.Convolution(matrix=[1, 2, 1, 2, 4, 2, 1, 2, 1])
+
+    diffevref = core.std.PlaneStats(evref, evref_rm, prop='EdgeValRef')
+    diffnext = core.std.PlaneStats(rgb, rgb.std.DeleteFrames([0]), prop='YNext')
+    diffprev = core.std.PlaneStats(rgb, rgb[0] + rgb, prop='YPrev')
+
+    db_clips = [
+        vsdpir(rgb, strength=st, mode='deblock', cuda=cuda, **vsdpir_args)
+        .std.SetFrameProp('Adb_DeblockStrength', intval=int(st)) for st in strs
+    ]
+
+    debl = core.std.FrameEval(
+        rgb, partial(_eval_db, clip=rgb, db_clips=db_clips, nthrs=nthrs),
+        prop_src=[diffevref, diffnext, diffprev]
+    )
+
+    return core.resize.Bicubic(debl, format=clip.format.id, matrix=matrix if not is_rgb else None)
