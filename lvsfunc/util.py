8788880e7d378f87ed445d240422cb3e70d34852
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2022-06-07 19:55:10 +0200

    replace_ranges: New functionalities, error/warning handling (#116)
    * replace_ranges: Add error/warning checking, mismatch=True
    
    * replace_ranges: Add alias note
    
    * replace_ranges: mypy, splicing note
    
    * replace_ranges: Catch plugin errors
    
    Makes errors seem less ???
    
    * replace_ranges: Add `exclusive` param
    
    * replace_ranges: Indenting

--- lvsfunc/util.py
+++ lvsfunc/util.py
@@ -169,10 +169,13 @@ def normalize_ranges(clip: vs.VideoNode, ranges: Range | List[Range]) -> List[Tu
 def replace_ranges(clip_a: vs.VideoNode,
                    clip_b: vs.VideoNode,
                    ranges: Range | List[Range] | None,
+                   exclusive: bool = False,
                    use_plugin: bool = True) -> vs.VideoNode:
     """
     A replacement for ReplaceFramesSimple that uses ints and tuples rather than a string.
-    Frame ranges are inclusive. Optionally strings can still be used.
+    Frame ranges are inclusive. This behaviour can be changed by setting `exclusive=True`.
+
+    If you're trying to splice in clips, it's recommended you use `vsutil.insert_clip` instead.
 
     This function will try to call the `VapourSynth-RemapFrames` plugin before doing any of its own processing.
     This should come with a speed boost, so it's recommended you install it.
@@ -186,44 +189,61 @@ def replace_ranges(clip_a: vs.VideoNode,
         * ``replace_ranges(black, white, [(200, -1)])``: replace 200 until the end with ``white``,
           leaving 1 frame of ``black``
 
+    Alias for this function is `lvsfunc.rfs`.
+
     Dependencies: VapourSynth-RemapFrames
 
-    :param clip_a:      Original clip
-    :param clip_b:      Replacement clip
-    :param ranges:      Ranges to replace clip_a (original clip) with clip_b (replacement clip).
+    :param clip_a:          Original clip
+    :param clip_b:          Replacement clip
+    :param ranges:          Ranges to replace clip_a (original clip) with clip_b (replacement clip).
 
-                        Integer values in the list indicate single frames,
+                            Integer values in the list indicate single frames,
 
-                        Tuple values indicate inclusive ranges.
+                            Tuple values indicate inclusive ranges.
 
-                        Negative integer values will be wrapped around based on clip_b's length.
+                            Negative integer values will be wrapped around based on clip_b's length.
 
-                        None values are context dependent:
+                            None values are context dependent:
 
-                            * None provided as sole value to ranges: no-op
-                            * Single None value in list: Last frame in clip_b
-                            * None as first value of tuple: 0
-                            * None as second value of tuple: Last frame in clip_b
-    :param use_plugin:  Use the ReplaceFramesSimple plugin for the rfs call.
+                                * None provided as sole value to ranges: no-op
+                                * Single None value in list: Last frame in clip_b
+                                * None as first value of tuple: 0
+                                * None as second value of tuple: Last frame in clip_b
+    :param exclusive:       Use exclusive ranges (Default: False).
+    :param use_plugin:      Use the ReplaceFramesSimple plugin for the rfs call (Default: True).
 
-    :return:           Clip with ranges from clip_a replaced with clip_b
+    :return:                Clip with ranges from clip_a replaced with clip_b
     """
     if ranges is None:
         return clip_a
 
+    if isinstance(ranges, str):  # type:ignore[unreachable]
+        raise ValueError("replace_ranges: 'This function does not take strings! Please use a list of tuples or ints!")
+
+    if clip_a.num_frames != clip_b.num_frames:
+        warnings.warn("replace_ranges: "
+                      f"'The number of frames ({clip_a.num_frames} vs. {clip_b.num_frames}) do not match! "
+                      "The function will still work, but you may run into unintended errors with the output clip!'")
+
     nranges = normalize_ranges(clip_b, ranges)
 
     if use_plugin and hasattr(core, 'remap'):
-        return core.remap.ReplaceFramesSimple(clip_a, clip_b, mappings=' '.join(f'[{s} {e}]' for s, e in nranges))
+        try:
+            return core.remap.ReplaceFramesSimple(clip_a, clip_b, mismatch=True,
+                                                  mappings=' '.join(f'[{s} {e}]' for s, e in nranges))
+        except vs.Error as e:
+            raise RuntimeError("replace_ranges: 'Some kind of error occured when running the plugin!\n"
+                               f"This was the error: \"{str(e)[21:]}\".\n"
+                               "If you don't know how to fix this, consider setting `use_plugin=False`.'")
 
     out = clip_a
 
     for start, end in nranges:
-        tmp = clip_b[start:end + 1]
+        tmp = clip_b[start:end + exclusive]
         if start != 0:
             tmp = out[: start] + tmp
         if end < out.num_frames - 1:
-            tmp = tmp + out[end + 1:]
+            tmp = tmp + out[end + exclusive:]
         out = tmp
 
     return out
