2e13cad18d9a722d52ea077a396d0d082d0db159
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2022-06-24 06:13:20 +0200

    Add dependency installation method

---
+++ lvsfunc/__main__.py
@@ -0,0 +1,71 @@
+import subprocess
+import sys
+import os
+from typing import List
+
+import pkg_resources
+
+
+class InstallationException(Exception):
+    ...
+
+pkg = 'lvsfunc'
+
+vsrepo_deps: List[str] = [
+    'akarin',
+    'BM3D',
+    'Descale',
+    'EEDI3 (obsolete)',
+    'fillborders',
+    'fmtconv',
+    'havsfunc',
+    'kagefunc',
+    'lsmas',
+    'NNEDI3CL',
+    'RemapFrames',
+    'rgsf',
+    'Scxvid',
+    'TDeintMod',
+    'TIVTC',
+    'vs-placebo',
+    'ZNEDI3',
+]
+
+pypi_deps: List[str] = [
+]
+
+
+def prompt_user() -> bool:
+    prompt = input(f"Install/update {pkg} dependencies? [y/n]: ").lower().strip()
+
+    match prompt:
+        case 'y' | 'yes': return True
+        case 'n' | _: return False
+
+
+def main() -> None:
+    if not prompt_user():
+        exit()
+
+    pkg_version = pkg_resources.get_distribution(pkg).version
+    pkg_ghrepo = "https://github.com/Irrational-Encoding-Wizardry/lvsfunc.git"
+
+    update_reqs = [sys.executable, '-m', 'pip', 'install']
+    update_reqs.append(f"git+{pkg_ghrepo}@v{pkg_version}")
+
+    vsrepo = ['vsrepo', 'install']
+    vsrepo.extend(vsrepo_deps)
+    vsrepo.extend(['--stub-output-file', '@'])
+
+    try:
+        subprocess.run(update_reqs, cwd=os.getcwd())
+
+        if vsrepo_deps:
+            subprocess.run(['vsrepo', 'update'])
+            subprocess.run(vsrepo)
+    except subprocess.CalledProcessError as e:
+        raise InstallationException(e.output)
+
+
+if __name__ == '__main__':
+    main()
