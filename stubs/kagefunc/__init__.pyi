54ce1d2634c44c9b2b72a0ae2e57d5cbbe15d691
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-08-08 22:29:57 +0200

    Update stubs

--- stubs/kagefunc/__init__.pyi
+++ stubs/kagefunc/__init__.pyi
@@ -1,3 +1,25 @@
 # flake8: noqa
 
-from .kagefunc import *
+from typing import Sequence
+
+import vapoursynth as vs
+
+
+def kirsch(src: vs.VideoNode) -> vs.VideoNode: ...
+
+
+def retinex_edgemask(src: vs.VideoNode, sigma: float | Sequence[float] = 1.0) -> vs.VideoNode: ...
+
+
+def squaremask(clip: vs.VideoNode, width: int, height: int, offset_x: int, offset_y: int) -> vs.VideoNode: ...
+
+
+def hardsubmask_fades(
+    clip: vs.VideoNode, ref: vs.VideoNode, expand_n: int = 8, highpass: int = 5000
+) -> vs.VideoNode: ...
+
+
+def hardsubmask(clip: vs.VideoNode, ref: vs.VideoNode, expand_n: int | None = None) -> vs.VideoNode: ...
+
+
+def crossfade(clipa: vs.VideoNode, clipb: vs.VideoNode, duration: int) -> vs.VideoNode: ...
