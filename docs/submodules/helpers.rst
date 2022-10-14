ff909794742ac58af5245f7eb13fd993b870fe2f
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-10-14 22:18:32 +0200

    Finish moving to vsdeinterlace

--- docs/submodules/helpers.rst
+++ docs/submodules/helpers.rst
@@ -7,7 +7,6 @@ These can be used by users as well.
 
 .. autosummary::
 
-    lvsfunc.helpers._calculate_dar_from_props
     lvsfunc.helpers._check_has_nvidia
 
 .. automodule:: lvsfunc.helpers
--- lvsfunc/helpers.py
+++ lvsfunc/helpers.py
@@ -2,13 +2,8 @@ from __future__ import annotations
 
 import subprocess as sp
 
-from vstools import FramePropError, get_prop, vs
-
-from .types import Dar
-
 __all__ = [
-    '_check_has_nvidia',
-    '_calculate_dar_from_props',
+    '_check_has_nvidia'
 ]
 
 
@@ -19,20 +14,3 @@ def _check_has_nvidia() -> bool:
         return True
     except sp.CalledProcessError:
         return False
-
-
-def _calculate_dar_from_props(clip: vs.VideoNode) -> Dar:
-    """Determine what DAR the clip is by checking default SAR props."""
-    frame = clip.get_frame(0)
-
-    try:
-        sar = get_prop(frame, "_SARDen", int), get_prop(frame, "_SARNum", int)
-    except FramePropError as e:
-        raise FramePropError(
-            "PARser", "", f"SAR props not found! Make sure your video indexing plugin sets them!\n\t{e}"
-        )
-
-    match sar:
-        case (11, 10) | (9, 8): return Dar.FULLSCREEN
-        case (33, 40) | (27, 32): return Dar.WIDESCREEN
-        case _: raise ValueError("Could not calculate DAR. Please set the DAR manually.")
