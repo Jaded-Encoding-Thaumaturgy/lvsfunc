56f3df5c1609a1b1b36a601687d4189437c723b5
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2023-01-29 12:08:09 +0100

    New function: custom_mask_clip

--- lvsfunc/__init__.py
+++ lvsfunc/__init__.py
@@ -9,10 +9,11 @@
 
 # flake8: noqa
 
-from . import comparison, deblock, exceptions, misc, render, types, util
+from . import comparison, deblock, exceptions, mask, misc, render, types, util
 from .comparison import *
 from .deblock import *
 from .exceptions import *
+from .mask import *
 from .misc import *
 from .render import *
 from .types import *
