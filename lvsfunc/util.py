79989a8637ad8712b0d95994a2c8b3c08d3e887c
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2024-02-03 18:53:28 +0100

    get_match_centers_scaling: New function (#165)
    * match_centers_formula: new util function
    
    * match_centers_formula: Expand docstring
    
    * get_match_centers_scaling: rename function, expand docs, minor optimisations
    
    * get_match_centers_scaling: Updates based on review

--- lvsfunc/util.py
+++ lvsfunc/util.py
@@ -5,11 +5,14 @@ import random
 import re
 from typing import Any
 
-from vstools import CustomIndexError, CustomValueError, FrameRangeN, FrameRangesN, core, vs
+from vstools import (CustomIndexError, CustomValueError, FrameRangeN,
+                     FrameRangesN, FuncExceptT, KwargsT,
+                     check_variable_resolution, core, get_h, get_w, vs)
 
 __all__ = [
     'colored_clips',
     'convert_rfs',
+    'get_match_centers_scaling',
 ]
 
 
@@ -98,3 +101,96 @@ def convert_rfs(rfs_string: str) -> FrameRangesN:
             ranges += [tuple(int(x) for x in str(match).strip().split(' '))]  # type:ignore[list-item]
 
     return ranges
+
+
+def get_match_centers_scaling(
+    clip: vs.VideoNode,
+    target_width: int | None = None,
+    target_height: int | None = 720,
+    func_except: FuncExceptT | None = None
+) -> KwargsT:
+    """
+    Convenience function to calculate the native resolution for sources that were upsampled
+    using the "match centers" model as opposed to the more common "match edges" models.
+
+    While match edges will align the edges of the outermost pixels in the target image,
+    match centers will instead align the *centers* of the outermost pixels.
+
+    Here's a visual example for a 3x1 image upsampled to 9x1:
+
+        * Match edges:
+
+    +-----------+-----------+-----------+
+    |     .     |     .     |     .     |
+    +-----------+-----------+-----------+
+    ↓                                   ↓
+    +---+---+---+---+---+---+---+---+---+
+    | . | . | . | . | . | . | . | . | . |
+    +---+---+---+---+---+---+---+---+---+
+
+        * Match centers:
+
+    +-----------+-----------+-----------+
+    |     .     |     .     |     .     |
+    +-----------+-----------+-----------+
+      ↓                               ↓
+    +---+---+---+---+---+---+---+---+---+
+    | . | . | . | . | . | . | . | . | . |
+    +---+---+---+---+---+---+---+---+---+
+
+    For a more detailed explanation, refer to this page: `<https://entropymine.com/imageworsener/matching/>`.
+
+    The formula for calculating values we can use during desampling is simple:
+
+    * width: clip.width * (target_width - 1) / (clip.width - 1)
+    * height: clip.height * (target_height - 1) / (clip.height - 1)
+
+    Example usage:
+
+    .. code-block:: python
+
+        >>> from vodesfunc import DescaleTarget
+        >>> ...
+        >>> native_res = get_match_centers_scaling(src, 1280, 720)
+        >>> rescaled = DescaleTarget(kernel=Catrom, upscaler=Waifu2x, downscaler=Hermite(linear=True), **native_res)
+
+    The output is meant to be passed to `vodesfunc.DescaleTarget` as keyword arguments,
+    but it may also apply to other functions that require similar parameters.
+
+    :param clip:            The clip to base the calculations on.
+    :param target_width:    Target width for the descale. This should probably be equal to the base width.
+                            If not provided, this value is calculated using the `target_height`.
+                            Default: None.
+    :param target_height:   Target height for the descale. This should probably be equal to the base height.
+                            If not provided, this value is calculated using the `target_width`.
+                            Default: 720.
+    :param func_except:     Function returned for custom error handling.
+                            This should only be set by VS package developers.
+
+    :return:                A dictionary with the keys, {width, height, base_width, base_height},
+                            which can be passed directly to `vodesfunc.DescaleTarget` or similar functions.
+    """
+
+    func = func_except or get_match_centers_scaling
+
+    if target_width is None and target_height is None:
+        raise CustomValueError("Either `target_width` or `target_height` must be a positive integer.", func)
+
+    if target_width is not None and (not isinstance(target_width, int) or target_width <= 0):
+        raise CustomValueError("`target_width` must be a positive integer or None.", func)
+
+    if target_height is not None and (not isinstance(target_height, int) or target_height <= 0):
+        raise CustomValueError("`target_height` must be a positive integer or None.", func)
+
+    check_variable_resolution(clip, func)
+
+    if target_height is None:
+        target_height = get_h(target_width, clip, 1)
+
+    elif target_width is None:
+        target_width = get_w(target_height, clip, 1)
+
+    width = clip.width * (target_width - 1) / (clip.width - 1)
+    height = clip.height * (target_height - 1) / (clip.height - 1)
+
+    return KwargsT(width=width, height=height, base_width=target_width, base_height=target_height)
