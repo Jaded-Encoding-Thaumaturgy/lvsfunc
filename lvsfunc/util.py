976a091dde7288a5cfa6615884eff14ee9a1e053
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2024-09-19 01:07:34 -0700

    Remove packets submodule (#169)
    * Remove packets submodule
    
    * Remove get_file_from_clip
    
    Would be implemented in `vstools` once the PR gets accepted.

--- lvsfunc/util.py
+++ lvsfunc/util.py
@@ -3,19 +3,17 @@ from __future__ import annotations
 import colorsys
 import random
 import re
-from typing import Any, Literal, overload
+from typing import Any
 
-from stgpytools import MISSING, FileWasNotFoundError, SPathLike
 from vstools import (CustomIndexError, CustomValueError, FrameRangeN,
-                     FrameRangesN, FuncExceptT, KwargsT, SPath,
-                     check_variable_resolution, core, fallback, get_h,
-                     get_prop, get_w, vs)
+                     FrameRangesN, FuncExceptT, KwargsT,
+                     check_variable_resolution, core, fallback, get_h, get_w,
+                     vs)
 
 __all__ = [
     'colored_clips',
     'convert_rfs',
     'get_match_centers_scaling',
-    'get_file_from_clip',
 ]
 
 
@@ -199,66 +197,3 @@ def get_match_centers_scaling(
     height = clip.height * (target_height - 1) / (clip.height - 1)
 
     return KwargsT(width=width, height=height, base_width=target_width, base_height=target_height)
-
-
-@overload
-def get_file_from_clip(
-    clip: vs.VideoNode, fallback: SPathLike | None = ...,
-    prop: str = ..., strict: bool = True,
-    func_except: FuncExceptT | None = ...
-) -> SPath:
-    ...
-
-
-@overload
-def get_file_from_clip(  # type:ignore[misc]
-    clip: vs.VideoNode, fallback: SPathLike | None = ...,
-    prop: str = ..., strict: bool = False,
-    func_except: FuncExceptT | None = ...
-) -> SPath | Literal[False]:
-    ...
-
-
-def get_file_from_clip(
-    clip: vs.VideoNode, fallback: SPathLike | None = None,
-    prop: str = "idx_filepath", strict: bool = False,
-    func_except: FuncExceptT | None = None
-) -> SPath | Literal[False]:
-    """
-    Helper function to get the file path from a clip.
-
-    This function also checks to ensure the file exists,
-    and throws an error if it doesn't.
-
-    :param clip:                    The clip to get the file path from.
-    :param fallback:                Fallback file path to use if the `prop` is not found.
-    :param prop:                    The property to get the file path from.
-                                    Default: "idx_filepath" (used by vs-source classes).
-    :param strict:                  If True, will raise an error if the `prop` is not found.
-                                    This makes it so the function will NEVER return False.
-                                    Default: False.
-    :param func_except:             Function returned for custom error handling.
-                                    This should only be set by VS package developers.
-
-    :raises FileWasNotFoundError:   The file path was not found.
-    :raises FramePropError:         The property was not found in the clip.
-    """
-
-    func = fallback(func_except, get_file_from_clip)
-
-    if fallback is not None and not (fallback_path := SPath(fallback)).exists() and strict:
-        raise FileWasNotFoundError("Fallback file not found!", func, fallback_path.absolute())
-
-    if not (path := get_prop(clip, prop, str, default=MISSING if strict else False, func=func)):
-        return fallback_path or False
-
-    if not (spath := SPath(str(path))).exists() and not fallback:
-        raise FileWasNotFoundError("File not found!", func, spath.absolute())
-
-    if spath.exists():
-        return spath
-
-    if fallback is not None and fallback_path.exists():
-        return fallback_path
-
-    raise FileWasNotFoundError("File not found!", func, spath.absolute())
