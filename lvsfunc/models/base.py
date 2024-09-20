d3e3169b247f0ba75f6e47046f1db6a572bd8c81
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2024-09-20 15:33:10 +0200

    New submodule: models (models to come)

---
+++ lvsfunc/models/base.py
@@ -0,0 +1,34 @@
+import importlib.resources as pkg_resources
+
+from stgpytools import FileWasNotFoundError, SPath
+
+__all__: list[str] = []
+
+
+def _get_pkg_path() -> SPath:
+    """Get the path of the package."""
+
+    import lvsfunc
+
+    return SPath(pkg_resources.files(lvsfunc)) / 'models' / 'shaders'
+
+
+def _get_model_path(sub_dir: str, model_name: str, fp16: bool = True) -> SPath:
+    """Get the path of the model."""
+
+    if model_name not in _model_paths:
+        raise FileWasNotFoundError(f"Model {model_name} not found!", _get_model_path)
+
+    model_path = _get_pkg_path() / str(sub_dir) / _model_paths[model_name]
+
+    if not fp16:
+        return model_path
+
+    return model_path.with_name(model_path.stem.split('_fp32')[0] + '_fp16.onnx')
+
+
+# Paths to every model. Should always end with _fp32.onnx, we swap it out later if necessary.
+_model_paths: dict[str, SPath] = {
+    # Delowpassing models
+    'LHzDelowpass.DoubleTaps_4_4_15_15': '1x_lanczos_hz_delowpass_4_4_15_15_fp32.onnx',
+}
